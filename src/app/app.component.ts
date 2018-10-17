2baded4f473a579f3ddf95e45985e146b5ce0eb2
Boubacar S. BARRY <bouba@hackages.io>
2018-10-17 16:33:16 +0200

    refactor(all): Starting point component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,58 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 // Use mock data
-// import { books as mockBooks } from './mocks/books';
-import { Book } from './models/book';
+import {books as mockBooks} from './mocks/books';
+import {Book} from './models/book';
 
 @Component({
   selector: 'app-book-store',
   styleUrls: ['app.component.css'],
-  templateUrl: 'app.component.html'
+  template: `
+    <!-- Start: Header component -->
+    <h1>{{title}}</h1>
+    <hr>
+    <!-- End: Header component -->
+
+    <!-- Start: List of books component -->
+    <div id="search-component">
+      <h4>Search in your library</h4>
+      <input id="search-box" #searchInput (input)="search($event)"/>
+    </div>
+    <!-- End: List of books component -->
+
+    <!-- Start: List of books component -->
+    <h3>All Books ({{books.length}})</h3>
+    <br>
+    <hr>
+    <div class="grid grid-pad" [class]="myClass">
+      <a class="col-1-4" *ngFor="let book of books">
+        <div class="module hero">
+          <h4>{{book.title}}</h4>
+        </div>
+      </a>
+    </div>
+    <!-- End: List of books component -->
+  `
 })
+
 export class AppComponent {
-  title = 'Bookstore by Hackages';
+  title = 'Bookstore by Hackages 2';
+  isValid = true;
+
+  myClass = 'lol';
 
   // Use mock data
-  books: Book[] = [];
+  books: Book[] = mockBooks;
 
-  search(term) {
+  search(medol) {
+    console.log(this);
+
+    const lol = function () {
+      console.log('this', this);
+    }.bind(this);
+
+    lol();
+
     // Implement the search function
+    console.log('search', medol);
   }
 }
